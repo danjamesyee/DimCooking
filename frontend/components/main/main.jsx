f7d23f825311ff2c88b94a48354dcd52cb5c29e3
Daniel Yee <danjamesyee@gmail.com>
2020-06-24 13:19:33 -0700

    Added splash image and stylings

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -1,10 +1,28 @@
 
 import React from 'react';
 
-const MainPage = (props) => (
-    <div>
+class MainPage extends React.Component{
+  
+    constructor(props) {
+        super(props);
+    }
 
-    </div>
-);
+    render () {
+        // const background = {
+        //     width: '100px',
+        //     height: '100px',
+        
+        //     backgroundImage: "url('https://dimcooking-dev.s3-us-west-1.amazonaws.com/15.%E6%99%B6%E7%91%A9%E9%AE%AE%E8%9D%A6%E9%A4%832880x1920_Gz5GCtr.jpg')"
+        // };style={background}
+        return (
+            <div>
+                <div className="splash" >
+                    <img src="https://dimcooking-dev.s3-us-west-1.amazonaws.com/15.%E6%99%B6%E7%91%A9%E9%AE%AE%E8%9D%A6%E9%A4%832880x1920_Gz5GCtr.jpg" width="100px" height="50px"/>
+                </div>
+            </div>
+        )
+    }
+ 
+};
 
 export default MainPage;
