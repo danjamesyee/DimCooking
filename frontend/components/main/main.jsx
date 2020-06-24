4515f7a8c101648afdc88ddcf1c859aa6e4d3a33
Daniel Yee <danjamesyee@gmail.com>
2020-06-24 14:02:12 -0700

    Added splash page on home

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -8,16 +8,26 @@ class MainPage extends React.Component{
     }
 
     render () {
-        // const background = {
-        //     width: '100px',
-        //     height: '100px',
-        
-        //     backgroundImage: "url('https://dimcooking-dev.s3-us-west-1.amazonaws.com/15.%E6%99%B6%E7%91%A9%E9%AE%AE%E8%9D%A6%E9%A4%832880x1920_Gz5GCtr.jpg')"
-        // };style={background}
         return (
             <div>
                 <div className="splash" >
                     <img src="https://dimcooking-dev.s3-us-west-1.amazonaws.com/15.%E6%99%B6%E7%91%A9%E9%AE%AE%E8%9D%A6%E9%A4%832880x1920_Gz5GCtr.jpg" width="100px" height="50px"/>
+                    <div>
+                        <span className="splash-name">
+                            <br/>
+                            <br />
+                            <br/>
+                            <div className="splash-title">Shrimp Dumplings(蝦餃)</div>
+                            <br />
+                            <div>A simple Dim Sum classic</div>
+                            <br />
+                            <div className='splash-text'>Daniel Yee</div>
+                        </span>
+                        <span className="dot">
+                            <div className='dot-text'>RECIPE</div>
+                            <div className='dot-text'>OF THE DAY</div>
+                        </span>
+                    </div>
                 </div>
             </div>
         )
