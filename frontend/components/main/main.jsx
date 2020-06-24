03a9eb4c227e6112d8cc7126f8f96620e2a9fa3b
Daniel Yee <danjamesyee@gmail.com>
2020-06-24 14:14:03 -0700

    Fixed stylings on home splash

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -12,7 +12,7 @@ class MainPage extends React.Component{
             <div>
                 <div className="splash" >
                     <img src="https://dimcooking-dev.s3-us-west-1.amazonaws.com/15.%E6%99%B6%E7%91%A9%E9%AE%AE%E8%9D%A6%E9%A4%832880x1920_Gz5GCtr.jpg" width="100px" height="50px"/>
-                    <div>
+                    <div className="splash-box">
                         <span className="splash-name">
                             <br/>
                             <br />
