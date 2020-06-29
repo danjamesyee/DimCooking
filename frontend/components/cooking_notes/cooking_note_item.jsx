fae439abb2ee3ea2e2cc87a109d67f6d679ea448
Daniel Yee <danjamesyee@gmail.com>
2020-06-29 15:23:35 -0700

    Added dates to the cooking notes

--- frontend/components/cooking_notes/cooking_note_item.jsx
+++ frontend/components/cooking_notes/cooking_note_item.jsx
@@ -32,7 +32,53 @@ class CookingNoteItem extends React.Component {
         } else {
             buttons = <div></div>
         } 
-
+        let date = cookingnote.created_at;
+        let today = new Date().toJSON();
+        let timeBetweenMinutes = today.slice(14,16) - date.slice(14,16)
+        let timeBetweenHours = today.slice(11,13) - date.slice(11,13);
+        let timeBetweenDays = today.slice(8,10) - date.slice(8,10)
+        let timeBetweenMonths = today.slice(5,7) - date.slice(5,7)
+        let timeBetweenYears = today.slice(0,4) - date.slice(0,4)
+        let time;
+        if (timeBetweenYears === 0) {
+            if (timeBetweenMonths === 0) {
+                if (timeBetweenDays === 0) {
+                    if (timeBetweenHours === 0) {
+                        if (timeBetweenMinutes != 1) {
+                            time = `${timeBetweenMinutes} minutes ago`;
+                        } else {
+                            time = `${timeBetweenMinutes} minute ago`
+                        }
+                    } else {
+                        if (timeBetweenHours != 1) {
+                            time = `${timeBetweenHours} hours ago`;
+                        } else {
+                            time = `${timeBetweenHours} hour ago`;
+                        }
+                    }
+                } else {
+                    if (timeBetweenDays != 1) {
+                        time = `${timeBetweenDays} days ago`;
+                    } else {
+                        time = `${timeBetweenDays} day ago`;
+                    }
+                    
+                }
+            } else {
+                if (timeBetweenMonths != 1) {
+                    time = `${timeBetweenMonths} months ago`
+                } else {
+                    time = `${timeBetweenMonths} month ago`
+                }
+                
+            }
+        } else {
+            if (timeBetweenYears != 1) {
+                time = `${timeBetweenYears} years ago`;
+            } else {
+                time = `${timeBetweenYears} year ago`;
+            }
+        }
         return (
             <div>
                 <div>
@@ -40,7 +86,8 @@ class CookingNoteItem extends React.Component {
                         <div className='note-prof'>
                         <span className="prof-dot"></span>
                         <div className="note-item">
-                            <h4>{cookingnote.username}</h4>
+                                <h4>{cookingnote.username}  <div className="time">|&nbsp;&nbsp;{time}</div></h4>
+                            
                             <h5>{cookingnote.body}</h5>
                             {buttons}
                         </div>
