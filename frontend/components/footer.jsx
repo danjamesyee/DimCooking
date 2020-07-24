45bd94acd6a5d26fb849c47edb277abf494aeeb1
Daniel Yee <danjamesyee@gmail.com>
2020-07-23 17:56:08 -0700

    Changes footer description

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => (
-   
-        <div className='footer'>
-            <div className='flexer'>
-                
-                <div className='foot-label'>  
-                    <h1 >ABOUT US</h1>
-                    Dim Cooking is a clone of the New York Times Cooking website.
-                    It was created by Daniel Yee, a software engineer based in San
-                    Francisco, CA. 
-                </div>
-                <div className="learn">
-                    <h1 className='foo'>LEARN MORE</h1>
-                    <div><a href="https://github.com/danjamesyee"><div>Github</div></a></div>
-                    <div><a href="https://www.linkedin.com/in/daniel-yee-6886441b0/"><div>LinkedIn</div></a></div>
-                    <div><a href="https://cooking.nytimes.com/">Original Site</a></div>
-                    
-                    <div><a href="https://thewoksoflife.com/">Original Recipes</a></div>
-                    
-                </div>
-            </div>
+  <div className="footer">
+    <div className="flexer">
+      <div className="foot-label">
+        <h1>ABOUT US</h1>
+        Dim Cooking is heavily inspired by the New York Times Cooking website.
+        It was created by Daniel Yee, a software engineer based in San
+        Francisco, CA.
+      </div>
+      <div className="learn">
+        <h1 className="foo">LEARN MORE</h1>
+        <div>
+          <a href="https://github.com/danjamesyee">
+            <div>Github</div>
+          </a>
         </div>
-        
-    
-)
+        <div>
+          <a href="https://www.linkedin.com/in/daniel-yee-6886441b0/">
+            <div>LinkedIn</div>
+          </a>
+        </div>
+        <div>
+          <a href="https://cooking.nytimes.com/">Original Site</a>
+        </div>
 
+        <div>
+          <a href="https://thewoksoflife.com/">Original Recipes</a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Footer;
