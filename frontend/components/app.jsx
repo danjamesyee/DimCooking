7337d2ad4023f3a4cb9abbe4b8e545a424f446ba
Daniel Yee <danjamesyee@gmail.com>
2020-08-03 11:09:51 -0700

    Builds out searchbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,38 +1,39 @@
 import React from "react";
 import GreetingContainer from "./greeting/greeting_container";
-import Modal from './modal/modal';
-import { Link, Route, Switch } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
-import NotFound from './routes/notFoundPage';
-import MainPage from './main/main_container'
-import RecipePage from './recipe/recipe_container';
-import Footer from './footer';
-import SavedRecipeContainer from './saved_recipes/saved_recipe_container'
-
+import Modal from "./modal/modal";
+import { Link, Route, Switch } from "react-router-dom";
+import { AuthRoute } from "../util/route_util";
+import NotFound from "./routes/notFoundPage";
+import MainPage from "./main/main_container";
+import RecipePage from "./recipe/recipe_container";
+import Footer from "./footer";
+import SavedRecipeContainer from "./saved_recipes/saved_recipe_container";
+import Search from "../components/search_bar/search_container";
 
 const App = () => (
-    <div className="cheese">
-        <Modal />
-        
-            <div className="main-nav">
-                <div className='hider'>
-                <Link to="/" className="main-title" style={{ textDecoration: 'none' }}><strong >點 | Dim Cooking</strong></Link>
-                </div>
-                <div className="search"></div>
-                <GreetingContainer className='greeting'/>  
-            </div>
-            
-            
-            
-        
-        <Switch>
-                <Route path='/' exact component={MainPage}/>
-                <Route path='/recipes/:recipeId' component={RecipePage}/>
-                <Route path='/saved_recipes' component={SavedRecipeContainer}/>
-                <Route component={NotFound} />
-        </Switch>
-        <Footer />
+  <div className="cheese">
+    <Modal />
+
+    <div className="main-nav">
+      <div className="hider">
+        <Link to="/" className="main-title" style={{ textDecoration: "none" }}>
+          <strong>點 | Dim Cooking</strong>
+        </Link>
+      </div>
+      
+
+      <Search />
+      <GreetingContainer className="greeting" />
     </div>
+
+    <Switch>
+      <Route path="/" exact component={MainPage} />
+      <Route path="/recipes/:recipeId" component={RecipePage} />
+      <Route path="/saved_recipes" component={SavedRecipeContainer} />
+      <Route component={NotFound} />
+    </Switch>
+    <Footer />
+  </div>
 );
 
 export default App;
